a8651dbeb209c375700630b089ad479eaa2adc85
CRISTIAN <CRISTIAN@DESKTOP-P5F05KU>
2026-09-18 22:07:03 -0500

    se agrego una carpeta de POO con una clase llamada Estudiante con 3 parametro privados que son id, nombre y edad

---
+++ src/co/poligran/paradigmas/edu/POO/Estudiante.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.poligran.paradigmas.edu.POO;
+
+/**
+ *
+ * @author CRISTIAN
+ */
+public class Estudiante {
+    private int id;
+    private String nombre;
+    private int edad;
+
+    public Estudiante(int id, String nombre, int edad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+
+    
+    
+}
--- src/co/poligran/paradigmas/edu/vista/ParadigmasPOO.java
+++ src/co/poligran/paradigmas/edu/vista/ParadigmasPOO.java
@@ -2,7 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package paradigmaspoo;
+package co.poligran.paradigmas.edu.vista;
+import co.poligran.paradigmas.edu.POO.Estudiante;
 
 /**
  *
@@ -14,7 +15,8 @@ public class ParadigmasPOO {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-System.out.println("Hola mundo"); 
+        Estudiante alum1 = new Estudiante(5,"German",18);
+System.out.println("hola mundo"); 
     }
     
 }
